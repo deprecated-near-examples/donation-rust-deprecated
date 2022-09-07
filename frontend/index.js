d832a48d9682cec1b1432069d2f266993c14def3
gagdiez <gagdiez.c@gmail.com>
2022-09-07 17:10:12 +0200

    updated wallet

--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,10 @@ import { Contract } from './near-interface';
 import { Wallet } from './near-wallet'
 
 // create the Wallet and the Contract
-window.wallet = new Wallet({contractId: process.env.CONTRACT_NAME});
-window.contract = new Contract({wallet: window.wallet})
+const wallet = new Wallet({contractId: process.env.CONTRACT_NAME, createAccessKey: true});
+const contract = new Contract({wallet: wallet})
 
-//  Setup on page load
+// Setup on page load
 window.onload = async () => {
   const isSignedIn = await wallet.startUp();
 
