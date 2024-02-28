4d9480bb2da7b975bff6a3884606967cd7fac32a
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2024-02-28 20:56:59 +0200

    contract

--- contract/src/donation.rs
+++ contract/src/donation.rs
@@ -1,79 +1,98 @@
+use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
+use near_sdk::serde::Serialize;
+use near_sdk::{env, log, near_bindgen, require, AccountId, NearSchema, NearToken, Promise};
+
+pub const STORAGE_COST: NearToken = NearToken::from_millinear(1);
+
 use crate::Contract;
 use crate::ContractExt;
 
-use near_sdk::serde::Serialize;
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, log, near_bindgen, AccountId, Promise, Balance};
-use near_sdk::json_types::U128;
-
-pub const STORAGE_COST: u128 = 1_000_000_000_000_000_000_000;
-
-#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[derive(NearSchema, BorshDeserialize, BorshSerialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
+#[abi(json, borsh)]
 pub struct Donation {
-  pub account_id: AccountId, 
-  pub total_amount: U128,
+    pub account_id: AccountId,
+    pub total_amount: NearToken,
 }
 
 #[near_bindgen]
 impl Contract {
-  #[payable] // Public - People can attach money
-  pub fn donate(&mut self) -> U128 {
-    // Get who is calling the method and how much $NEAR they attached
-    let donor: AccountId = env::predecessor_account_id();
-    let donation_amount: Balance = env::attached_deposit();
+    #[payable]
+    pub fn donate(&mut self) -> String {
+        // Get who is calling the method and how much NEAR they attached
+        let donor: AccountId = env::predecessor_account_id();
+        let donation_amount = env::attached_deposit();
 
-    let mut donated_so_far = self.donations.get(&donor).unwrap_or(0);
+        require!(
+            donation_amount > STORAGE_COST,
+            format!(
+                "Attach at least {} yoctoNEAR to cover for the storage cost",
+                STORAGE_COST
+            )
+        );
 
-    let to_transfer: Balance = if donated_so_far == 0 {
-      // This is the user's first donation, lets register it, which increases storage
-      assert!(donation_amount > STORAGE_COST, "Attach at least {} yoctoNEAR", STORAGE_COST);
+        let mut donated_so_far: NearToken = self
+            .donations
+            .get(&donor)
+            .unwrap_or(NearToken::from_near(0));
 
-      // Subtract the storage cost to the amount to transfer
-      donation_amount - STORAGE_COST
-    }else{
-      donation_amount
-    };
+        let to_transfer = if donated_so_far.is_zero() {
+            // This is the user's first donation, lets register it, which increases storage
+            // Subtract the storage cost to the amount to transfer
+            donation_amount.saturating_sub(STORAGE_COST).to_owned()
+        } else {
+            donation_amount
+        };
 
-    // Persist in storage the amount donated so far
-    donated_so_far += donation_amount;
-    self.donations.insert(&donor, &donated_so_far);
-    
-    log!("Thank you {} for donating {}! You donated a total of {}", donor.clone(), donation_amount, donated_so_far);
-    
-    // Send the money to the beneficiary
-    Promise::new(self.beneficiary.clone()).transfer(to_transfer);
+        // Persist in storage the amount donated so far
+        donated_so_far = donated_so_far.saturating_add(donation_amount);
 
-    // Return the total amount donated so far
-    U128(donated_so_far)
-  }
+        self.donations.insert(&donor, &donated_so_far);
 
-  // Public - get donation by account ID
-  pub fn get_donation_for_account(&self, account_id: AccountId) -> Donation {
-    Donation {
-      account_id: account_id.clone(),
-      total_amount: U128(self.donations.get(&account_id).unwrap_or(0))
+        log!(
+            "Thank you {} for donating {}! You donated a total of {}",
+            donor.clone(),
+            donation_amount,
+            donated_so_far
+        );
+
+        // Send the NEAR to the beneficiary
+        Promise::new(self.beneficiary.clone()).transfer(to_transfer);
+
+        // Return the total amount donated so far
+        donated_so_far.to_string()
     }
-  }
 
-  // Public - get total number of donors
-  pub fn number_of_donors(&self) -> u64 {
-    self.donations.len()
-  }
+    // Public Method - get donation by account ID
+    pub fn get_donation_for_account(&self, account_id: AccountId) -> Donation {
+        Donation {
+            account_id: account_id.clone(),
+            total_amount: self
+                .donations
+                .get(&account_id)
+                .unwrap_or(NearToken::from_near(0)),
+        }
+    }
 
-  // Public - paginate through all donations on the contract
-  pub fn get_donations(&self, from_index: Option<U128>, limit: Option<u64>) -> Vec<Donation> {
-    //where to start pagination - if we have a from_index, we'll use that - otherwise start from 0 index
-    let start = u128::from(from_index.unwrap_or(U128(0)));
+    // Public Method - get total number of donors
+    pub fn number_of_donors(&self) -> u64 {
+        self.donations.len()
+    }
 
-    //iterate through donation
-    self.donations.keys()
-      //skip to the index we specified in the start variable
-      .skip(start as usize) 
-      //take the first "limit" elements in the vector. If we didn't specify a limit, use 50
-      .take(limit.unwrap_or(50) as usize) 
-      .map(|account| self.get_donation_for_account(account))
-      //since we turned map into an iterator, we need to turn it back into a vector to return
-      .collect()
-  }
+    // Public Method - paginate through all donations on the contract
+    pub fn get_donations(&self, from_index: Option<u64>, limit: Option<u64>) -> Vec<Donation> {
+        // where to start pagination - if we have a from_index, we'll use that - otherwise start from 0 index
+        let start = from_index.unwrap_or(0);
+
+        self.donations
+            .into_iter()
+            .skip(start as usize)
+            .take(limit.unwrap_or(10) as usize)
+            .map(|(account_id, total_amount)| Donation {
+                account_id,
+                total_amount,
+            })
+            .collect()
+    }
 }
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,102 +1,117 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId};
-use near_sdk::collections::{UnorderedMap};
+// Find all our documentation at https://docs.near.org
+use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::{near_bindgen, AccountId, NearSchema, NearToken};
 
 mod donation;
 
+// Define the contract structure
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(NearSchema, BorshDeserialize, BorshSerialize)]
+#[borsh(crate = "near_sdk::borsh")]
+#[abi(json, borsh)]
 pub struct Contract {
-  pub beneficiary: AccountId,
-  pub donations: UnorderedMap<AccountId, u128>,
+    pub beneficiary: AccountId,
+    pub donations: UnorderedMap<AccountId, NearToken>,
 }
 
+// Define the default, which automatically initializes the contract
 impl Default for Contract {
-  fn default() -> Self {
-    Self{
-      beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap(),
-      donations: UnorderedMap::new(b"d"),
+    fn default() -> Self {
+        Self {
+            beneficiary: "v2.faucet.nonofficial.testnet".parse().unwrap(),
+            donations: UnorderedMap::new(b"d"),
+        }
     }
-  }
 }
 
+// Implement the contract structure
 #[near_bindgen]
 impl Contract {
-  #[init]
-  #[private] // Public - but only callable by env::current_account_id()
-  pub fn init(beneficiary: AccountId) -> Self {
-    Self {
-      beneficiary,
-      donations: UnorderedMap::new(b"d"),
+    // Public Method - but only callable by env::current_account_id()
+    // initializes the contract with a beneficiary
+    #[init]
+    #[private]
+    pub fn init(beneficiary: AccountId) -> Self {
+        Self {
+            beneficiary,
+            donations: UnorderedMap::new(b"d"),
+        }
     }
-  }
 
-  // Public - beneficiary getter
-  pub fn get_beneficiary(&self) -> AccountId {
-    self.beneficiary.clone()
-  }
+    // Public Method - get the current beneficiary
+    pub fn get_beneficiary(&self) -> &AccountId {
+        &self.beneficiary
+    }
 
-  // Public - but only callable by env::current_account_id(). Sets the beneficiary
-  #[private]
-  pub fn change_beneficiary(&mut self, beneficiary: AccountId) {
-    self.beneficiary = beneficiary;
-  }
+    // Public Method - but only callable by env::current_account_id()
+    // sets the beneficiary
+    #[private]
+    pub fn change_beneficiary(&mut self, beneficiary: AccountId) {
+        self.beneficiary = beneficiary;
+    }
 }
 
-
+/*
+ * The rest of this file holds the inline tests for the code above
+ * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+ */
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use near_sdk::testing_env;
-  use near_sdk::test_utils::VMContextBuilder;
-  use near_sdk::Balance;
+    use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::testing_env;
+    use near_sdk::NearToken;
 
-  const BENEFICIARY: &str = "beneficiary";
-  const NEAR: u128 = 1000000000000000000000000;
+    const BENEFICIARY: &str = "beneficiary";
+    const ONE_NEAR: NearToken = NearToken::from_near(1);
 
-  #[test]
-  fn initializes() {
-      let contract = Contract::init(BENEFICIARY.parse().unwrap());
-      assert_eq!(contract.beneficiary, BENEFICIARY.parse().unwrap())
-  }
+    #[test]
+    fn initializes() {
+        let contract = Contract::init(BENEFICIARY.parse().unwrap());
+        assert_eq!(
+            contract.beneficiary,
+            BENEFICIARY.parse::<AccountId>().unwrap().to_string()
+        );
+    }
 
-  #[test]
-  fn donate() {
-      let mut contract = Contract::init(BENEFICIARY.parse().unwrap());
+    #[test]
+    fn donate() {
+        let mut contract = Contract::init(BENEFICIARY.parse().unwrap());
 
-      // Make a donation
-      set_context("donor_a", 1*NEAR);
-      contract.donate();
-      let first_donation = contract.get_donation_for_account("donor_a".parse().unwrap());
+        // Make a donation
+        set_context("donor_a", ONE_NEAR);
+        contract.donate();
+        let first_donation = contract.get_donation_for_account("donor_a".parse().unwrap());
 
-      // Check the donation was recorded correctly
-      assert_eq!(first_donation.total_amount.0, 1*NEAR);
+        // Check the donation was recorded correctly
+        assert_eq!(first_donation.total_amount, ONE_NEAR);
 
-      // Make another donation
-      set_context("donor_b", 2*NEAR);
-      contract.donate();
-      let second_donation = contract.get_donation_for_account("donor_b".parse().unwrap());
+        // Make another donation
+        set_context("donor_b", ONE_NEAR.saturating_mul(2));
+        contract.donate();
+        let second_donation = contract.get_donation_for_account("donor_b".parse().unwrap());
 
-      // Check the donation was recorded correctly
-      assert_eq!(second_donation.total_amount.0, 2*NEAR);
+        // Check the donation was recorded correctly
+        assert_eq!(second_donation.total_amount, ONE_NEAR.saturating_mul(2));
 
-      // User A makes another donation on top of their original
-      set_context("donor_a", 1*NEAR);
-      contract.donate();
-      let first_donation = contract.get_donation_for_account("donor_a".parse().unwrap());
+        // User A makes another donation on top of their original
+        set_context("donor_a", ONE_NEAR);
+        contract.donate();
+        let first_donation = contract.get_donation_for_account("donor_a".parse().unwrap());
 
-      // Check the donation was recorded correctly
-      assert_eq!(first_donation.total_amount.0, 1*NEAR * 2);
+        // Check the donation was recorded correctly
+        assert_eq!(first_donation.total_amount, ONE_NEAR.saturating_mul(2));
 
-      assert_eq!(contract.number_of_donors(), 2);
-  }
+        assert_eq!(contract.number_of_donors(), 2);
+    }
 
-  // Auxiliar fn: create a mock context
-  fn set_context(predecessor: &str, amount: Balance) {
-    let mut builder = VMContextBuilder::new();
-    builder.predecessor_account_id(predecessor.parse().unwrap());
-    builder.attached_deposit(amount);
+    // Auxiliar fn: create a mock context
+    fn set_context(predecessor: &str, amount: NearToken) {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor.parse().unwrap());
+        builder.attached_deposit(amount);
 
-    testing_env!(builder.build());
-  }
+        testing_env!(builder.build());
+    }
 }
